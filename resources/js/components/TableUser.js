161c24b2ce1278188196d2e76ee4b432947d4f53
Sony Oyarzun <Sony.oyarzun@gmail.com>
2020-08-16 04:09:11 -0400

    ok

--- resources/js/components/TableUser.js
+++ resources/js/components/TableUser.js
@@ -1,9 +1,9 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 
 import axios from 'axios'
 
 //import datable
-import { MDBDataTable, MDBNavLink } from 'mdbreact';
+import { MDBDataTableV5, MDBBadge } from 'mdbreact';
 
 //importar modals
 import EditUser from './modals/EditUser'
@@ -14,89 +14,101 @@ import DeleteUser from './modals/DeleteUser'
 
 class TableUser extends Component {
 
-   
-  constructor(props){
+
+  constructor(props) {
     super(props);
     this.state = {
-        users:[]
+      users: []
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
 
-    axios.get('user').then(response=>{
-      this.setState({users:response.data})
-    }).catch(error=>{
-      alert("Error "+error)
+    axios.get('user').then(response => {
+      this.setState({ users: response.data })
+    }).catch(error => {
+      alert("Error " + error)
     })
 
   }
 
 
-render() {
+  render() {
 
     const data = {
-        columns: [
-            {
-              label: 'ID',
-              field: 'id',
-              width: 150,
-              attributes: {
-                'aria-controls': 'DataTable',
-                'aria-label': 'ID',
-              },
-            },
-             {
-              label: 'Nombre',
-              field: 'name',
-              width: 150,
-            },
-            {
-              label: 'Email',
-              field: 'email',
-              width: 200,
-            },
-            {
-              label: 'Rol',
-              field: 'role',
-              sort: 'asc',
-              width: 100,
-            }
-          ],
-        rows: [
-            ...this.state.users.map((data, i) => (
-                {
-                    id: data.id,
-                    name: data.name,
-                    email: data.email,
-                    role: data.role
-
-                }
-            ))
-        ]
+      columns: [
+        {
+          label: 'ID',
+          field: 'id',
+          width: 150,
+          attributes: {
+            'aria-controls': 'DataTable',
+            'aria-label': 'ID',
+          },
+        },
+        {
+          label: 'Nombre',
+          field: 'name',
+          width: 150,
+        },
+        {
+          label: 'Email',
+          field: 'email',
+          width: 200,
+        },
+        {
+          label: 'Rol',
+          field: 'role',
+          sort: 'asc',
+          width: 100,
+        },
+        {
+          label: 'Editar',
+          field: 'edit',
+          width: 100,
+        },
+        {
+          label: 'Contraseña',
+          field: 'pass',
+          width: 100,
+        },
+        {
+          label: 'Eliminar',
+          field: 'delete',
+          width: 100,
+        }
+      ],
+      rows: [
+        ...this.state.users.map((data, order) => (
+          {
+            id: data.id,
+            name: data.name,
+            email: data.email,
+            role: data.role,
+            edit: <EditUser name={data.name}/>,
+            pass: <PassUser/>,
+            delete: <DeleteUser/>
+          }
+        ))
+      ]
     };
 
-    console.log(data,'data2')
+
     return (
-        <div>
-            <section className="tanning">
-                <div className="container">
-                    <h1>Customer Details</h1>
-                    <div className="introductory_details customer-table">
-                        <MDBDataTable
-                            className='cust-table'
-                            responsive
-                            bordered
-                            hover
-                            btn
-                            data={data}
-                        />
-                    </div>
-                </div>
-            </section>
-        </div>
+      <div>
+
+        <MDBDataTableV5
+          className='cust-table'
+          responsive
+          bordered
+          hover
+          btn
+          data={data}
+        />
+
+      </div>
     )
-}
+  }
 
 }
 
