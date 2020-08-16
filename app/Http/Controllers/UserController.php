8e633d57d9ce5656a1026e4819e43ce7297fb39c
Sony Oyarzun <Sony.oyarzun@gmail.com>
2020-08-16 16:12:21 -0400

    ok

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+
+
 class UserController extends Controller
 {
     public function index()
@@ -16,4 +20,71 @@ class UserController extends Controller
            return json_encode($users);
 
     }
+
+    public function insert(Request $request)
+    {
+        $product    = $request->id;
+        $quantity   = $request->quantity;
+        $send       = $request->send;
+
+                DB::table('user')->insert([
+                    'id' => $send,
+                    'name' => $product,
+                    'mail' => $quantity,
+                    'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                ]);
+
+    }
+
+    public function update(Request $request)
+    {
+        $product    = $request->id;
+        $quantity   = $request->quantity;
+        $send       = $request->send;
+
+                DB::table('user')->insert([
+                    'id' => $send,
+                    'name' => $product,
+                    'mail' => $quantity,
+                    'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                ]);
+
+    }
+
+    public function changePass(Request $request)
+    {
+        $product    = $request->id;
+        $quantity   = $request->quantity;
+        $send       = $request->send;
+
+                DB::table('user')->insert([
+                    'id' => $send,
+                    'name' => $product,
+                    'mail' => $quantity,
+                    'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                ]);
+
+    }
+
+    public function delete(Request $request)
+    {
+        $product = $request->product;
+        $send    = $request->send;
+
+  
+                 if(isset($request->product)){ 
+                 DB::table('lines')->where('id_product', $product)->delete();
+                 }
+               
+                 if(isset($request->send)){ 
+                 DB::table('sends')->where('id', $send)->delete();
+                 }
+            
+        
+    }
+
+
 }
