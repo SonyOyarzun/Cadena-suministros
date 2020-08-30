9d087d8ddf94ddab611a43a1faf1b9a2f617abc2
SonyOyarzun <46294866+SonyOyarzun@users.noreply.github.com>
2020-08-29 20:36:02 -0400

    Update ChainController.php

--- app/Http/Controllers/ChainController.php
+++ app/Http/Controllers/ChainController.php
@@ -6,14 +6,35 @@ use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\Auth;
 use App\Chain;
+use App\User;
 
 class ChainController extends Controller
 {
     public function index()
     {
-		$chain = Chain::get();
+    $chain = Chain::get();
 
-		return $chain;
+    $send  = [];
+    foreach($chains as $chain){
+
+    $from = User::findOrFail($chain->from);
+    $to   = User::findOrFail($chain->from);
+
+    $send = [
+      "id"          => $chain->id,
+      "transaction" => $chain->id,
+      "from"        => $chain->id,
+      "to"          => $chain->id,
+      "state"       => $chain->id,
+      "updated_at"  => User::findOrFail($chain->from),
+      "created_at"  => User::findOrFail($chain->to),
+    ];
+    }
+
+   
+   
+
+		return 
 
     }
     
