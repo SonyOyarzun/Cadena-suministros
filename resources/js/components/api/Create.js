3103fb7508f5c7c6fb718e66e45f25d37d9f10f0
SonyOyarzun <46294866+SonyOyarzun@users.noreply.github.com>
2020-09-10 20:52:42 -0300

    ok

--- resources/js/components/api/Create.js
+++ resources/js/components/api/Create.js
@@ -57,7 +57,7 @@ function Create(props) {
     if (isset(props.getData) || isset(props.getUserSend)){
 
       const userSend = props.getUserSend.values
-      const data = props.getData
+      const transaction = props.getData
 
       const info = {
         from: user.name,
@@ -72,7 +72,7 @@ function Create(props) {
 
       const tx = BigchainDB.Transaction.makeCreateTransaction(
         // Data JSON
-        { data },
+        { transaction },
 
         { info: info },
 
