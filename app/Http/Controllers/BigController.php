7d7350ee76a87a5fae9df680444c1eda8d3c21d6
SonyOyarzun <46294866+SonyOyarzun@users.noreply.github.com>
2020-09-10 20:48:33 -0300

    Update BigController.php

--- app/Http/Controllers/BigController.php
+++ app/Http/Controllers/BigController.php
@@ -36,7 +36,7 @@ class BigController extends Controller
 			$body = $response->getBody()->getContents();
 		} catch (\Exception $e) {
 			//return json_encode($e->getMessage());
-			return $e->getMessage();
+			return [];
 		}
 			return $body;
 	}
@@ -51,7 +51,7 @@ class BigController extends Controller
 			$body = $response->getBody()->getContents();
 		} catch (\Exception $e) {
 			//return json_encode($e->getMessage());
-			return $e->getMessage();
+			return [];
 		}
 			return $body;
 	}
