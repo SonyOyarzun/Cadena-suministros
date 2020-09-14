6c00fd435ce9b1da72db4339d43fe92fd9a86765
SonyOyarzun <46294866+SonyOyarzun@users.noreply.github.com>
2020-09-14 04:46:54 -0300

    Update BigController.php

--- app/Http/Controllers/BigController.php
+++ app/Http/Controllers/BigController.php
@@ -16,7 +16,7 @@ class BigController extends Controller
 
 		try {
 			$client = new Client();
-			$response = $client->request('GET', 'https://test.ipdb.io/api/v1/transactions?asset_id='.$request->asset.'&operation=CREATE&operation=TRANSFER&last_tx=false');
+			$response = $client->request('GET', 'https://test.ipdb.io/api/v1/transactions?asset_id='.$request->asset.'&last_tx=false');
 			$statusCode = $response->getStatusCode();
 			$body = $response->getBody()->getContents();
 		} catch (\Exception $e) {
