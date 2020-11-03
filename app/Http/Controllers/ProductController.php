a0234a5dd729ce3902496a480f29db9cc99bb69c
SonyOyarzun <46294866+SonyOyarzun@users.noreply.github.com>
2020-11-03 19:59:24 -0300

    OK

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\product;
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -22,9 +22,19 @@ class ProductController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+        if (!isset($request->name)) {
+            return ['message'=>'Debe ingresar nombre','type'=>'error'];
+          } else {
+      
+            $product = new Product;
+            $product->JSON  = $request->JSON;
+            $product->created_at = now();
+            $product->updated_at = now();
+            $product->save();
+      
+            return ['message'=>'Producto ingresado','type'=>'success'];
     }
 
     /**
