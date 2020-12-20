20ab76d4e9432341b4ec5c4c33d5d6a3f682c589
SonyOyarzun <46294866+SonyOyarzun@users.noreply.github.com>
2020-12-19 21:02:16 -0300

    ok

--- resources/js/components/extra/PushNotification.js
+++ resources/js/components/extra/PushNotification.js
@@ -1,72 +1,100 @@
 import React, { Component, Fragment, useState, useRef } from 'react';
 import ReactDOM from 'react-dom';
-import fire from './FireBase'
+import { getConfig } from "../tables/TableFunctions";
 
 function PushNotification(props) {
 
-    const messaging = fire.messaging();
+    let config = {};
 
-    messaging
-        .requestPermission()
-        .then(function () {
-            return messaging.getToken()
+    getConfig()
+        .then(response => {
+
+          //  console.log('firebaseConfig', response)
+
+            config = {
+                apiKey: response.apiKey,
+                authDomain: response.authDomain,
+                databaseURL: response.databaseURL,
+                projectId: response.projectId,
+                storageBucket: response.storageBucket,
+                messagingSenderId: response.messagingSenderId,
+                appId: response.appId,
+                measurementId: response.measurementId
+            };
+
+
+
         })
-        .then(function (token) {
-            console.log(token)
+        .catch(error => {
+            console.log('firebaseConfig error:', error)
+        })
+        .finally(() => {
 
-            const params = {
-                fcm_token: token
-            }
+            const fire = firebase.initializeApp(config);
+            const messaging = fire.messaging();
 
-            axios
-                .post('/save-device-token', params, {
-                    headers: {
-                        Authorization: `Bearer ${localStorage.usertoken}`
+            messaging
+                .requestPermission()
+                .then(function () {
+                    return messaging.getToken()
+                })
+                .then(function (token) {
+                    console.log(token)
+
+                    const params = {
+                        fcm_token: token
                     }
-                })
-                .then(response => {
-                    console.log(response.data)
-
-                })
-                .catch(err => {
-                    console.log(err)
-                })
-
-                .finally(() => {
 
                     axios
-                        .post('/send-push', props, {
+                        .post('/save-device-token', params, {
                             headers: {
                                 Authorization: `Bearer ${localStorage.usertoken}`
                             }
                         })
                         .then(response => {
                             console.log(response.data)
-                            return response.data
+
                         })
                         .catch(err => {
                             console.log(err)
                         })
 
+                        .finally(() => {
+
+                            axios
+                                .post('/send-push', props, {
+                                    headers: {
+                                        Authorization: `Bearer ${localStorage.usertoken}`
+                                    }
+                                })
+                                .then(response => {
+                                    console.log(response.data)
+                                    return response.data
+                                })
+                                .catch(err => {
+                                    console.log(err)
+                                })
+
+                        })
+
+
+                })
+                .catch(function (err) {
+                    console.log("Unable to get permission to notify.", err);
                 })
 
 
+
+            messaging.onMessage(function (payload) {
+                const noteTitle = payload.notification.title;
+                const noteOptions = {
+                    body: payload.notification.body,
+                    icon: payload.notification.icon,
+                }
+                new Notification(noteTitle, noteOptions);
+            })
+
         })
-        .catch(function (err) {
-            console.log("Unable to get permission to notify.", err);
-        })
-
-
-
-    messaging.onMessage(function (payload) {
-        const noteTitle = payload.notification.title;
-        const noteOptions = {
-            body: payload.notification.body,
-            icon: payload.notification.icon,
-        }
-        new Notification(noteTitle, noteOptions);
-    })
-
 
 }
 
