a795cad7d5dbfcee96c6be958fae471b92a59c63
SonyOyarzun <46294866+SonyOyarzun@users.noreply.github.com>
2020-12-28 00:10:27 -0300

    OK

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -69,39 +69,67 @@ class UserController extends Controller
   public function new(Request $request)
   {
 
-    if (!isset($request->name)) {
-      return ['message'=>'Debe ingresar nombre','type'=>'error'];
-    } elseif (!isset($request->email)) {
-      return ['message'=>'Debe ingresar mail','type'=>'error'];
-    } elseif (!filter_var($request->email, FILTER_VALIDATE_EMAIL)) {
-      return ['message'=>'Formato mail no valido','type'=>'error'];
-    } elseif (User::where('email', $request->email)->exists()) {
-      return ['message'=>'Mail ya existe en los registros','type'=>'error'];
-    } elseif (!isset($request->role)) {
-      return ['message'=>'Debe ingresar rol','type'=>'error'];
-    } elseif (!isset($request->path)) {
-      return ['message'=>'Debe ingresar ruta de api','type'=>'error'];
-    } elseif (!isset($request->pass)) {
-      return ['message'=>'Debe ingresar contraseña','type'=>'error'];
-    } elseif (!preg_match('/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{8,12}$/', $request->pass)) {
-      return ['message'=>'Contraseña debe Contener: Mayúsculas, números y mas de 8 carácteres','type'=>'error'];
-    } elseif ($request->pass != $request->confirmPass) {
-      return ['message'=>'Contraseñas no coinciden','type'=>'error'];
-    } else {
+    try {
 
-      $user = new User;
-      $user->name  = $request->name;
-      $user->email = $request->email;
-      $user->role  = $request->role;
-      $user->path  = $request->path;
-      $user->publicKey   = $request->publicKey;
-      $user->privateKey  = $request->privateKey;
-      $user->password  = bcrypt($request->pass);
-      $user->created_at = now();
-      $user->updated_at = now();
-      $user->save();
+      if (!isset($request->name)) {
+        return ['message' => 'Debe ingresar nombre', 'type' => 'error'];
+      } elseif (!isset($request->email)) {
+        return ['message' => 'Debe ingresar mail', 'type' => 'error'];
+      } elseif (!filter_var($request->email, FILTER_VALIDATE_EMAIL)) {
+        return ['message' => 'Formato mail no valido', 'type' => 'error'];
+      } elseif (User::where('email', $request->email)->exists()) {
+        return ['message' => 'Mail ya existe en los registros', 'type' => 'error'];
+      } elseif (!isset($request->role)) {
+        return ['message' => 'Debe ingresar rol', 'type' => 'error'];
+      } elseif (!isset($request->path)) {
+        return ['message' => 'Debe ingresar ruta de api', 'type' => 'error'];
+      } elseif (!isset($request->pass)) {
+        return ['message' => 'Debe ingresar contraseña', 'type' => 'error'];
+      } elseif (!preg_match('/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{8,12}$/', $request->pass)) {
+        return ['message' => 'Contraseña debe Contener: Mayúsculas, números y mas de 8 carácteres', 'type' => 'error'];
+      } elseif ($request->pass != $request->confirmPass) {
+        return ['message' => 'Contraseñas no coinciden', 'type' => 'error'];
+      } else {
 
-      return ['message'=>'Usuario Creado','type'=>'success'];
+
+        $user = new User;
+        $user->name  = $request->name;
+        $user->email = $request->email;
+        $user->role  = $request->role;
+        $user->path  = $request->path;
+
+        $privateKey = openssl_pkey_new(array(
+          'private_key_bits' => 2048,      // Tamaño de la llave
+          'private_key_type' => OPENSSL_KEYTYPE_RSA,
+        ));
+
+        // Guardar la llave privada en el archivo private.key. No compartir este archivo con nadie
+        openssl_pkey_export_to_file($privateKey, 'private.key_' . $request->email);
+
+        // Generar la llave pública para la llave privada
+        $public_key = openssl_pkey_get_details($privateKey);
+
+        file_put_contents('public.key', $public_key['key']);
+
+        // Libera la llave privada
+        openssl_free_key($privateKey);
+
+
+
+        //$user->publicKey   = $request->publicKey;
+        //$user->privateKey  = $request->privateKey;
+        $user->publicKey   = $public_key;
+        $user->privateKey  = $privateKey;
+        $user->password  = bcrypt($request->pass);
+        $user->created_at = now();
+        $user->updated_at = now();
+        $user->save();
+
+        return ['message' => 'Usuario Creado', 'type' => 'success'];
+      }
+    } catch (\Throwable $th) {
+
+      return ['message' => $th->getMessage(), 'type' => 'error'];
     }
   }
 
@@ -110,19 +138,19 @@ class UserController extends Controller
     $user = User::findOrFail($request->id);
     if ($user == null) {
 
-      return ['message'=>'Usuario no encontrado','type'=>'error'];
+      return ['message' => 'Usuario no encontrado', 'type' => 'error'];
     } else {
 
       if (!isset($request->name)) {
-        return ['message'=>'Debe ingresar nombre','type'=>'error'];
+        return ['message' => 'Debe ingresar nombre', 'type' => 'error'];
       } elseif (!isset($request->email)) {
-        return ['message'=>'Debe ingresar mail','type'=>'error'];
+        return ['message' => 'Debe ingresar mail', 'type' => 'error'];
       } elseif (!filter_var($request->email, FILTER_VALIDATE_EMAIL)) {
-        return ['message'=>'Formato mail no valido','type'=>'error'];
+        return ['message' => 'Formato mail no valido', 'type' => 'error'];
       } elseif (!isset($request->role)) {
-        return ['message'=>'Debe ingresar role','type'=>'error'];
+        return ['message' => 'Debe ingresar role', 'type' => 'error'];
       } elseif (!isset($request->path)) {
-        return ['message'=>'Debe ingresar ruta de api','type'=>'error'];
+        return ['message' => 'Debe ingresar ruta de api', 'type' => 'error'];
       } else {
 
         $user->name  = $request->name;
@@ -131,7 +159,7 @@ class UserController extends Controller
         $user->path  = $request->path;
         $user->save();
 
-        return ['message'=>'Usuario Actualizado','type'=>'success'];
+        return ['message' => 'Usuario Actualizado', 'type' => 'success'];
       }
     }
   }
@@ -141,19 +169,19 @@ class UserController extends Controller
     $user = User::findOrFail($request->id);
     if ($user == null) {
 
-      return ['message'=>'Usuario no encontrado','type'=>'error'];
+      return ['message' => 'Usuario no encontrado', 'type' => 'error'];
     } else {
 
       if (!isset($request->pass)) {
-        return ['message'=>'Debe ingresar contraseña','type'=>'error'];
+        return ['message' => 'Debe ingresar contraseña', 'type' => 'error'];
       } elseif (!preg_match('/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{8,12}$/', $request->pass)) {
-        return ['message'=>'Contraseña debe Contener: Mayúsculas, números y mas de 8 carácteres','type'=>'error'];
+        return ['message' => 'Contraseña debe Contener: Mayúsculas, números y mas de 8 carácteres', 'type' => 'error'];
       } elseif ($request->pass != $request->confirmPass) {
-        return ['message'=>'Contraseñas no coinciden','type'=>'error'];
+        return ['message' => 'Contraseñas no coinciden', 'type' => 'error'];
       } else {
         $user->password  = bcrypt($request->pass);
         $user->save();
-        return ['message'=>'Contraseña Actualizada','type'=>'success'];
+        return ['message' => 'Contraseña Actualizada', 'type' => 'success'];
       }
     }
   }
@@ -164,14 +192,14 @@ class UserController extends Controller
       $user = User::findOrFail($request->id);
       if ($user == null) {
 
-        return ['message'=>'Usuario no encontrado','type'=>'error'];
+        return ['message' => 'Usuario no encontrado', 'type' => 'error'];
       } else {
         $user->delete();
-        return ['message'=>'Usuario Eliminado','type'=>'success'];
+        return ['message' => 'Usuario Eliminado', 'type' => 'success'];
         return $request->id;
       }
     } else {
-      return ['message'=>'Administrador no puede ser Eliminado','type'=>'warning'];
+      return ['message' => 'Administrador no puede ser Eliminado', 'type' => 'warning'];
     }
   }
 
@@ -182,39 +210,35 @@ class UserController extends Controller
     try {
 
       if (!isset($request->email)) {
-        return ['message'=>'Debe ingresar mail','type'=>'error'];
+        return ['message' => 'Debe ingresar mail', 'type' => 'error'];
       } elseif (!filter_var($request->email, FILTER_VALIDATE_EMAIL)) {
-        return ['message'=>'Formato mail no valido','type'=>'error'];
+        return ['message' => 'Formato mail no valido', 'type' => 'error'];
       } elseif (!isset($request->password)) {
-        return ['message'=>'Debe ingresar contraseña','type'=>'error'];
+        return ['message' => 'Debe ingresar contraseña', 'type' => 'error'];
       } else {
-  
+
         $user = User::where("email", $request->email)->first();
-  
+
         if (!is_null($user)) {
-  
+
           if (Hash::check($request->password, $user->password)) {
-  
+
             $credentials = $request->only('email', 'password');
-  
+
             Auth::attempt($credentials);
-  
+
             return true;
-  
           } else {
-  
-            return ['message'=>'Contraseña no coincide','type'=>'error'];
-  
+
+            return ['message' => 'Contraseña no coincide', 'type' => 'error'];
           }
-  
         } else {
-  
-          return ['message'=>'Mail no registrado','type'=>'error'];
-          
+
+          return ['message' => 'Mail no registrado', 'type' => 'error'];
         }
       }
     } catch (\Throwable $th) {
-     return ['message'=>'Error al Ingresar','type'=>'error'];
+      return ['message' => 'Error al Ingresar', 'type' => 'error'];
     }
   }
 }
--- server.php
+++ server.php
@@ -160,5 +160,25 @@ https://github.com/ashik0019/A-Quick-Guide-to-Implement-Firebase-Web-Push-Notifi
 
  AL CREAR USUARIO SI NO SE HACE REFRESH, CREA CON LA MISMA PUBLIC Y PRIVATE
 
+
  */
 
+/*
+VARIABLES SSL EN WINDOWS
+https://diego.com.es/ssl-y-openssl-en-php
+
+This may help if you are on windows:
+
+  Click on the START button
+  Click on CONTROL PANEL
+  Click on SYSTEM AND SECURITY
+  Click on SYSTEM
+  Click on ADVANCED SYSTEM SETTINGS
+  Click on ENVIRONMENT VARIABLES
+  Under "System Variables" click on "NEW"
+  Enter the "Variable name" OPENSSL_CONF
+  Enter the "Variable value". My is - C:\wamp\bin\apache\Apache2.2.17\conf\openssl.cnf
+  Click "OK" and close all the windows and RESTART your computer.
+  The OPENSSL should be correctly working.
+
+*/
